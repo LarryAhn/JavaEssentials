5c1acf5e631cd579eca08b75d25cad0432e2f69c
Lawrence Ahn <lawrenceahn27@gmail.com>
2016-11-16 10:56:28 +0900

    foreach example

--- src/main/java/com/zuperztarahn/example/ForEachExample.java
+++ src/main/java/com/zuperztarahn/example/ForEachExample.java
@@ -1,7 +1,9 @@
 package com.zuperztarahn.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Ahn on 2016. 11. 15..
@@ -9,9 +11,9 @@ import java.util.List;
 public class ForEachExample {
 
     public static void main(String[] args) {
-
         java8ForEachExample();
-
+        classStyleForEachAndMap();
+        java8ForEachAndMapLambdaExpression();
     }
 
     public static void java8ForEachExample() {
@@ -23,6 +25,44 @@ public class ForEachExample {
         language.forEach(System.out::println);
 
         language.forEach(v->System.out.println(v));
+        System.out.println();
+    }
+
+    public static void classStyleForEachAndMap() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        for (Map.Entry<String, Integer> entry : items.entrySet()) {
+            System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+        }
+
+        System.out.println();
+    }
+
+    public static void java8ForEachAndMapLambdaExpression() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        items.forEach((k,v)->System.out.println("Item : " + k + " Count : " + v));
+
+        items.forEach((k,v)->{
+            System.out.println("Item : " + k + " Count : " + v);
+            if("E".equals(k)){
+                System.out.println("Value E");
+            }
+        });
+
+        System.out.println();
     }
 
 }
